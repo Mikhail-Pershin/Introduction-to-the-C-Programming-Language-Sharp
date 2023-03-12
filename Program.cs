b9d4939dc8ff892b447609d64983ee107dfbc927
Mikhail_Pershin <mcpershin@yandex.ru>
2023-03-12 23:00:42 +0300

    add solution of task 4

--- Program.cs
+++ Program.cs
@@ -4,17 +4,44 @@
 // a = 2 b = 10 -> max = 10
 // // a = -9 b = -3 -> max = -3
 
+// Console.Write("Введите первое число: ");
+// int number_A = int.Parse(Console.ReadLine()!);
+// Console.Write("Введите второе число: ");
+// int number_B = int.Parse(Console.ReadLine()!);
+
+//   if (number_A > number_B)
+// {
+//     Console.WriteLine($"Первое число {number_A} больше чем второе {number_B}");
+// }
+
+//   else
+// {
+//     Console.WriteLine($"Второе число {number_B} больше чем первое {number_A}");
+// }
+
+
+// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
 Console.Write("Введите первое число: ");
-int number_A = int.Parse(Console.ReadLine()!);
+int number_1 = int.Parse(Console.ReadLine()!);
 Console.Write("Введите второе число: ");
-int number_B = int.Parse(Console.ReadLine()!);
+int number_2 = int.Parse(Console.ReadLine()!);
+Console.Write("Введите третье число: ");
+int number_3 = int.Parse(Console.ReadLine()!);
 
-  if (number_A > number_B)
+int max = number_1;
+
+if (number_2 > max)
+{ 
+  max = number_2;
+}
+if (number_3 > max)
 {
-    Console.WriteLine($"Первое число {number_A} больше чем второе {number_B}");
+  max = number_3;
 }
 
-  else
-{
-    Console.WriteLine($"Второе число {number_B} больше чем первое {number_A}");
-}
+Console.WriteLine($"Максимальное из этих чисел: {max}");
